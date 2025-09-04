dba54c17b58464d0159a001be6df2140719e19cc
wonzzu <wonzzu@naver.com>
2025-09-04 13:56:59 +0900

    feat:정렬기능 추가

--- src/main/java/com/picketlogia/picket/api/product/model/ProductSearchDto.java
+++ src/main/java/com/picketlogia/picket/api/product/model/ProductSearchDto.java
@@ -7,4 +7,5 @@ import lombok.Setter;
 @Setter
 public class ProductSearchDto {
     private String name;
+    private String genre;
 }
--- src/main/java/com/picketlogia/picket/api/sortoption/model/SortOption.java
+++ src/main/java/com/picketlogia/picket/api/sortoption/model/SortOption.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 @Getter
 public enum SortOption {
     NEWEST("최신순"),
-    DAILY_RANK("일간 랭킹순"),
-    WEEKLY_RANK("주간 랭킹순"),
-    MOST_REVIEWS("리뷰 많은순"),
-    NEAR_END("종료 임박순");
+    review("리뷰 많은 순"),
+    rating("평점 높은 순");
 
     private final String name;
 
