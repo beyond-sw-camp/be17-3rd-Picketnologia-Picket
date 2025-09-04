57a94d1ee542a8a007d69c15d8d595ea55a114e2
sangwoo <lesw1216@gmail.com>
2025-09-04 21:44:23 +0900

    refactor: cors 프론트 도메인 추가

--- src/main/java/com/picketlogia/picket/config/SecurityConfig.java
+++ src/main/java/com/picketlogia/picket/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://www.picket.o-r.kr"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
