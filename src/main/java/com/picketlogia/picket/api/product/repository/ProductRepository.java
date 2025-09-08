8065119df7d3456764b68e87d56beb7690dcabfc
wonzzu <wonzzu@naver.com>
2025-09-08 19:59:58 +0900

    refator:리뷰 갯수나 리뷰 평점순으로 정렬 오류 해결 (#14)

--- src/main/java/com/picketlogia/picket/api/product/repository/ProductRepository.java
+++ src/main/java/com/picketlogia/picket/api/product/repository/ProductRepository.java
@@ -27,11 +27,11 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImage WHERE p.genre.code = :code")
     Page<Product> findByGenre_Code(String code, Pageable pageable);
 
-    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImage WHERE p.genre.code = :code order by p.reviewCount desc")
-    Page<Product> findByGenre_CodeOrderByReviewCountDesc(String code, Pageable pageable);
-
-    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImage WHERE p.genre.code = :code order by p.reviewRating desc")
-    Page<Product> findByGenre_CodeOrderByReviewRatingDesc(String code, Pageable pageable);
+//    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImage WHERE p.genre.code = :code order by p.reviewCount desc")
+//    Page<Product> findByGenre_CodeOrderByReviewCountDesc(String code, Pageable pageable);
+//
+//    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImage WHERE p.genre.code = :code order by p.reviewRating desc")
+//    Page<Product> findByGenre_CodeOrderByReviewRatingDesc(String code, Pageable pageable);
 
     @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImage WHERE p.genre.code = :code order by p.createdAt desc")
     Page<Product> findByGenre_CodeOrderByCreatedAtDesc(String code, Pageable pageable);
