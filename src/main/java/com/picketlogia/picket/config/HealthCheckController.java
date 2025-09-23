7f22196458177736d674dbeac2751dc5c0fafc5e
sangwoo <lesw1216@gmail.com>
2025-09-23 15:42:27 +0900

    health-check 수정

--- src/main/java/com/picketlogia/picket/config/HealthCheckController.java
+++ src/main/java/com/picketlogia/picket/config/HealthCheckController.java
@@ -9,6 +9,6 @@ public class HealthCheckController {
 
     @GetMapping("/health-check")
     public ResponseEntity healthCheck() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok("Health Check OK");
     }
 }
