16c1cf36b0ea30f015bdd30e587408972455f1b0
seol <norsors@nate.com>
2025-09-23 16:46:29 +0900

    refactor:Blue Green 무중단 배포 test

--- src/main/java/com/picketlogia/picket/config/HealthCheckController.java
+++ src/main/java/com/picketlogia/picket/config/HealthCheckController.java
@@ -9,6 +9,6 @@ public class HealthCheckController {
 
     @GetMapping("/health-check")
     public ResponseEntity healthCheck() {
-        return ResponseEntity.ok("Health Check OK");
+        return ResponseEntity.ok("BLUE");
     }
 }
